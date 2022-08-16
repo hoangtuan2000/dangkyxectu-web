ec54bacf2578bc9ce9bab39108adf0df8afbc6ab
hoangtuan2000 <duonghoangtuan.td@gmail.com>
2022-08-16 15:39:04 +0700

    update dialog show car information

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -11,11 +11,13 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
+    Grid,
     List,
     ListItem,
     ListItemText,
     styled,
     Typography,
+    useTheme,
 } from "@mui/material";
 import NearMeIcon from "@mui/icons-material/NearMe";
 
@@ -59,6 +61,7 @@ const CardContainer = styled(Card)(({ theme }) => ({
 }));
 
 export default function Home() {
+    const theme = useTheme();
     const [open, setOpen] = React.useState(false);
     const handleClickOpen = () => () => {
         setOpen(true);
@@ -71,7 +74,11 @@ export default function Home() {
         <Box>
             {data.map((srcImage, index) => {
                 return (
-                    <CardContainer key={index} onClick={handleClickOpen()} data-aos="zoom-in">
+                    <CardContainer
+                        key={index}
+                        onClick={handleClickOpen()}
+                        data-aos="zoom-in"
+                    >
                         <CardActionArea>
                             <CardMedia
                                 component="img"
@@ -138,26 +145,84 @@ export default function Home() {
                 );
             })}
 
-            <Dialog open={open} onClose={handleClose} scroll="body">
-                <DialogTitle id="scroll-dialog-title">Subscribe</DialogTitle>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                scroll="body"
+                fullWidth={true}
+            >
+                <DialogTitle>Thông Tin Xe</DialogTitle>
                 <DialogContent>
-                    <DialogContentText
-                        id="scroll-dialog-description"
-                        tabIndex={-1}
-                    >
-                        {[...new Array(50)]
-                            .map(
-                                () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-                            )
-                            .join("\n")}
-                    </DialogContentText>
+                    <Box>
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsBl_xuk80F5PI3pXBK0L45rf652XU583ITA&usqp=CAU"
+                            style={{
+                                float: "left",
+                                width: "200px",
+                                height: "200px",
+                                objectFit: "cover",
+                                borderRadius: "10px",
+                                marginRight: "10px",
+                            }}
+                        />
+                    </Box>
+                    <Box>
+                        <Typography variant="p" component="div">
+                            Xe 16 chỗ
+                        </Typography>
+                        <Typography variant="p" component="div">
+                            Biển số: 65A-12345
+                        </Typography>
+                        <Typography variant="p" component="div">
+                            Tình trạng: Hoạt Động
+                        </Typography>
+                        <Button variant="contained" size="small">
+                            Đăng Ký Lịch Trình
+                        </Button>
+                        <Typography variant="p" component="div">
+                            Lịch trình:
+                        </Typography>
+                        <List
+                            sx={{
+                                bgcolor: "background.paper",
+                                padding: "0px",
+                                float: "left",
+                                borderRadius: "10px",
+                            }}
+                        >
+                            <ListItem>
+                                <NearMeIcon
+                                    fontSize="small"
+                                    sx={{ marginRight: "5px" }}
+                                />
+                                <ListItemText
+                                    primary="Đại Học Cần Thơ AAA SFD SDFD AA AA AA AA"
+                                    secondary="01/01/2022 - 03/01/2022"
+                                    primaryTypographyProps={{
+                                        fontSize: "13px",
+                                        fontWeight: "bold",
+                                        whiteSpace: "nowrap",
+                                        overflow: "hidden",
+                                        textOverflow: "ellipsis",
+                                    }}
+                                    secondaryTypographyProps={{
+                                        fontSize: "12px",
+                                        whiteSpace: "nowrap",
+                                        overflow: "hidden",
+                                        textOverflow: "ellipsis",
+                                    }}
+                                />
+                            </ListItem>
+                        </List>
+                    </Box>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Subscribe</Button>
+                    <Button
+                        onClick={handleClose}
+                        sx={{ color: theme.palette.error.light }}
+                    >
+                        Thoát
+                    </Button>
                 </DialogActions>
             </Dialog>
         </Box>
