c6d476a0ce4cc94ae1d7b5316839ab8af441e8ef
hoangtuan2000 <duonghoangtuan.td@gmail.com>
2022-08-18 17:11:55 +0700

    update UI DriverManagement.js

--- src/screens/driverManagement/DriverManagement.js
+++ src/screens/driverManagement/DriverManagement.js
@@ -3,6 +3,7 @@ import {
     Button,
     IconButton,
     Pagination,
+    Rating,
     Tooltip,
     useTheme,
 } from "@mui/material";
@@ -18,7 +19,9 @@ import {
 import React from "react";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
-
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
 
 const CustomNoRowsOverlay = () => {
     return (
@@ -31,48 +34,48 @@ const CustomNoRowsOverlay = () => {
 const rowsTest = [
     {
         id: 1,
-        name: "hình",
-        type: "46 chổ",
-        description: "65A - 123456",
-        startTime: "29/06/2022 07:00",
-        endTime: "30/06/2022 06:59",
-        status: "Deleted",
+        fullName: "Nguyễn Văn A",
+        code: "TX123",
+        numberPhone: "0123456789",
+        license: "Hạng C",
+        numberOfTrips: "20",
+        status: "Hoạt Động",
     },
     {
         id: 2,
-        name: "hình",
-        type: "46 chổ",
-        description: "65A - 123456",
-        startTime: "29/06/2022 07:00",
-        endTime: "30/06/2022 06:59",
-        status: "New",
+        fullName: "Nguyễn Văn A",
+        code: "TX123",
+        numberPhone: "0123456789",
+        license: "Hạng C",
+        numberOfTrips: "20",
+        status: "Hoạt Động",
     },
     {
         id: 3,
-        name: "hình",
-        type: "46 chổ",
-        description: "65A - 123456",
-        startTime: "29/06/2022 07:00",
-        endTime: "30/06/2022 06:59",
-        status: "Auditing",
+        fullName: "Nguyễn Văn A",
+        code: "TX123",
+        numberPhone: "0123456789",
+        license: "Hạng C",
+        numberOfTrips: "20",
+        status: "Hoạt Động",
     },
     {
         id: 4,
-        name: "hình",
-        type: "46 chổ",
-        description: "65A - 123456",
-        startTime: "29/06/2022 07:00",
-        endTime: "30/06/2022 06:59",
-        status: "Completed",
+        fullName: "Nguyễn Văn A",
+        code: "TX123",
+        numberPhone: "0123456789",
+        license: "Hạng C",
+        numberOfTrips: "20",
+        status: "Ngừng Hoạt Động",
     },
     {
         id: 5,
-        name: "hình",
-        type: "46 chổ",
-        description: "65A - 123456",
-        startTime: "29/06/2022 07:00",
-        endTime: "30/06/2022 06:59",
-        status: "Expired",
+        fullName: "Nguyễn Văn A",
+        code: "TX123",
+        numberPhone: "0123456789",
+        license: "Hạng C",
+        numberOfTrips: "20",
+        status: "Hoạt Động",
     },
 ];
 
@@ -80,7 +83,8 @@ function DriverManagement() {
     const theme = useTheme();
     // const [rows, setRows] = useState<GridRowsProp>([])
 
-    const status = ["Deleted", "New", "Auditing", "Completed", "Expired"];
+    const status = ["Hoạt Động", "Ngừng Hoạt Động"];
+
     // const type = ["Daily audit", "Audited by 5s"]
     const columns = [
         {
@@ -90,100 +94,120 @@ function DriverManagement() {
             sortable: false,
         },
         {
-            field: "name",
-            headerName: "Mã",
-            description: "Mã",
-            width: 100,
+            field: "fullName",
+            headerName: "Họ Tên",
+            description: "Họ Tên",
+            width: 150,
+            sortable: false,
+            renderCell: (params) => {
+                return (
+                    <Tooltip title={params.row.fullName} arrow>
+                        <span
+                            style={{
+                                whiteSpace: "nowrap",
+                                overflow: "hidden",
+                                textOverflow: "ellipsis",
+                            }}
+                        >
+                            {params.row.fullName}
+                        </span>
+                    </Tooltip>
+                );
+            },
+        },
+        {
+            field: "code",
+            headerName: "Mã Tài Xế",
+            description: "Mã Tài Xế",
+            width: 110,
             sortable: false,
         },
         {
-            field: "type",
-            headerName: "Họ Tên",
-            description: "Họ Tên",
+            field: "numberPhone",
+            headerName: "Số Điện Thoại",
+            description: "Số Điện Thoại",
             width: 120,
             sortable: false,
         },
         {
-            field: "description",
+            field: "license",
             headerName: "Giấy Phép Lái Xe",
             description: "Giấy Phép Lái Xe",
-            width: 180,
+            width: 140,
             sortable: false,
         },
         {
-            field: "status",
-            headerName: "Số Điện Thoại",
-            description: "Số Điện Thoại",
+            field: "numberOfTrips",
+            headerName: "Số Chuyến Đi",
+            description: "Số Chuyến Đi",
+            width: 110,
+            sortable: false,
+        },
+        {
+            field: "reviews",
+            headerName: "đánh giá",
+            description: "đánh giá",
             width: 165,
+            sortable: false,
+            renderCell: (params) => {
+                return (
+                    <Rating name="read-only" value={2} readOnly size="small" />
+                );
+            },
+        },
+        {
+            field: "status",
+            headerName: "trạng thái",
+            description: "trạng thái",
+            width: 125,
             sortable: false,
             renderCell: (params) => {
                 let bgColor = "white";
                 let textColor = "white";
                 switch (params.value) {
                     case status[0]:
-                        bgColor = "red";
-                        break;
-                    case status[1]:
-                        textColor = "black";
-                        break;
-                    case status[2]:
                         bgColor = "green";
                         break;
-                    case status[3]:
-                        bgColor = "blue";
-                        break;
-                    case status[4]:
+                    case status[1]:
                         bgColor = "gray";
                         break;
                 }
                 return (
-                    <>
-                        <Box
-                            sx={{
-                                backgroundColor: bgColor,
-                                color: textColor,
-                                padding: "2px 6px",
-                                borderRadius: "10px",
-                                fontSize: "12px",
-                                WebkitBoxShadow:
-                                    "3px 4px 15px -1px rgba(0,0,0,0.25)",
-                                boxShadow: "3px 4px 15px -1px rgba(0,0,0,0.25)",
-                            }}
-                        >
-                            {params.value}
-                        </Box>
-                    </>
+                    <Box
+                        style={{
+                            width: "110px",
+                            textAlign: "center",
+                            backgroundColor: bgColor,
+                            color: textColor,
+                            padding: "2px 6px",
+                            borderRadius: "10px",
+                            fontSize: "12px",
+                            WebkitBoxShadow:
+                                "3px 4px 15px -1px rgba(0,0,0,0.25)",
+                            boxShadow: "3px 4px 15px -1px rgba(0,0,0,0.25)",
+                        }}
+                    >
+                        {params.value}
+                    </Box>
                 );
             },
         },
         {
-            field: "total",
-            headerName: "Tổng Số Chuyến Đi",
-            description: "Tổng Số Chuyến Đi",
-            width: 180,
-            sortable: false,
-        },
-        {
-            field: "reviews",
-            headerName: "Đánh Giá",
-            description: "Đánh Giá",
-            width: 180,
-            sortable: false,
-        },
-        {
             field: "update",
-            headerName: "Cập Nhật",
-            description: "Cập Nhật",
-            width: 165,
+            headerName: "cập nhật",
+            description: "cập nhật",
+            width: 80,
             sortable: false,
             renderCell: (params) => {
-                return (
-                    <Tooltip title="Xem chi tiết" arrow>
-                        <IconButton color="primary">
-                            <VisibilityIcon />
-                        </IconButton>
-                    </Tooltip>
-                );
+                if (params.row.status != status[3]) {
+                    return (
+                        <Tooltip title="Cập Nhật" arrow>
+                            <IconButton color="primary">
+                                <ModeEditIcon />
+                            </IconButton>
+                        </Tooltip>
+                    );
+                }
             },
         },
     ];
@@ -241,7 +265,7 @@ function DriverManagement() {
                 autoHeight
                 disableColumnMenu
                 disableSelectionOnClick
-                pageSize={2}
+                pageSize={5}
                 // sx={{
                 //     ".MuiDataGrid-columnSeparator": {
                 //         display: "none"
